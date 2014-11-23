4b4486b0e390b5e12bf881e267c898942e2d93f3
Collin Cierlak <ccierlak92@gmail.com>
2014-11-22 22:56:13 -0800

    lab7 update
    matrix_gen prints binary now

--- Lab7/README.txt
+++ Lab7/README.txt
@@ -1,3 +1,4 @@
 Program creates random matrix in specified file passed in argv[1]
 
 11/22 wrote matrix_gen.c
+11/22 updated matrix_gen.c to write the numbers as binary
--- Lab7/matrix_gen.c
+++ Lab7/matrix_gen.c
@@ -4,12 +4,39 @@ int main(int argc, char **argv)
 {
 	int i;
 	int j;
+	int k;
+	signed char* bits;
+	signed char randNum;
 	int n = atoi(argv[1]);
-	for(i=0;i<n;i++){
-		for(j=0;j<n;j++){
-				printf("%d\t",100-rand()%200);
+	//n*n is size of matrix
+	for(i=0; i<n; i++) {
+		for(j=0; j<n; j++) {
+			//signed char to hold int as 8 bits
+			randNum = 100-rand()%200;
+			//change to 8 bit representation, little endian
+			if(randNum<0) {
+				//for negative numbers set negative bit and make number postive for modular operation
+				bits[0]=1;
+				randNum*=-1;
+			}
+			for(k=7; k>0; k--) {
+				if(randNum%2==1) {
+					bits[k]=1;
+				} else {
+
+					bits[k]=0;
+				}
+				//shift randNum right
+				randNum=randNum>>1;
+			}
+			for(k=0; k<8; k++) {
+				printf("%d",bits[k]);
+				//clear bit
+				bits[k]=0;
+			}
 		}
-		printf("\n");
 	}
+	//write null terminator
+	printf("\0");
 	return 0;
 }
